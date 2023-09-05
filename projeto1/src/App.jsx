513bbf8a1c74e5dd93837a6c95c58e1abfc63475
Victor <victormnuzzi003@gmail.com>
2023-09-05 19:09:17 -0300

    estrutura do projeto | ex a) e b)

---
+++ projeto1/src/App.jsx
@@ -0,0 +1,18 @@
+import { } from 'react'
+import Nav from './Nav'
+import Home from './Home'
+import Produtos from './Produtos'
+import Sobre from './Sobre'
+import Footer from './Footer'
+
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
