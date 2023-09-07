ea5c03226b11249bfbb5a07d0538480fc56d2b54
Henri Lopes <82175620+henrilopes1@users.noreply.github.com>
2023-09-07 15:27:25 -0300

    Update Produtos.jsx

--- projeto1/src/Produtos.jsx
+++ projeto1/src/Produtos.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Foto from "./img/45184mn.jpg"
 
+
 function Produtos() {
 
     return(
         <>
-        <img src= {Foto} alt="Img" title='Selfie'/>
-        <img src= {Foto} alt="Img" title='Selfie'/>
-        <img src= {Foto} alt="Img" title='Selfie'/>
-        <img src= {Foto} alt="Img" title='Selfie'/>
+        <h1>Selfie</h1>
+        <img src= {Foto} alt="Img"/>
+        
+        <h1>Selfie</h1>
+        <img src= {Foto} alt="Img"/>
+
+        <h1>Selfie</h1>
+        <img src= {Foto} alt="Img"/>
+
+        <h1>Selfie</h1>
+        <img src= {Foto} alt="Img"/>
+        
     
-            <p>4 imagens com seus titulos</p>
+        
         </>
     )
 }
